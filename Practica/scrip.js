d05fc0b2847c8b2408574e2fb6e47f624a69f5df
Arkant04 <delacorte19@gmail.com>
2023-12-21 11:29:59 +0100

    MOD: el ejercicio ya esta completado solo falta documentarlo

--- Practica/scrip.js
+++ Practica/scrip.js
@@ -1,19 +1,28 @@
+let texto = 0;
 function cambiarTexto(){
-  let texto = 0;
+    texto++;
  
-
-  if(texto == 0){
-    document.querySelector("#textoCambiante").innerHTML = "Has pulsado el boton"
-    texto++;
-  }
-
-  if (texto == 1){
-    texto++;
+    
+    
+    
+    if (texto == 1){
+        document.querySelector("#textoCambiante").innerHTML = "Has pulsado el boton"
   }
   
   if(texto == 2){
     document.querySelector("#textoCambiante").innerHTML = "Has vuelto ha pulsar el boton!!!"
-    texto++;
+  }
+
+  if (texto == 3){
+    document.querySelector("#textoAlternante").style.background = "black"
+    document.querySelector("#textoAlternante").style.color = "white"
+  }
+
+  if(texto == 4){
+    document.querySelector("#textoCambiante").innerHTML = "Pulsa el boton"
+    document.querySelector("#textoAlternante").style.background = "red"
+    document.querySelector("#textoAlternante").style.color = "black"
+    texto = 0
   }
 console.log(texto)
   
